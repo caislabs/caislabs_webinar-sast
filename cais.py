d4c160a3f4249aebd0a90c27449c61e61fa17b78
ffilho <fausto.almeida.f@gmail.com>
2021-05-19 11:25:42 -0300

    Quarto commit - Correção dos bugs apontados pelo SonarCloud

--- cais.py
+++ cais.py
@@ -18,16 +18,14 @@ app.config['SECRET_KEY'] = SECRET_KEY
 csrf = CSRFProtect()
 csrf.init_app(app)
 
-getpage = 'input.html'
-
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    return render_template('index.html', 200)
 
 @app.route('/aes', methods=['GET'])
 def aes():
     otp=None
-    return render_template(getpage, otp=otp, path='aes', title='AES', desc="Entre com a mensagem")
+    return render_template('input.html', 200, otp=otp, path='aes', title='AES', desc="Entre com a mensagem")
 
 @app.route('/aes', methods=['POST'])
 def aes_post(otp=None):
@@ -40,7 +38,7 @@ def aes_post(otp=None):
 @app.route('/ping', methods=['GET'])
 def ping():
     otp=None
-    return render_template(getpage, otp=otp, path='ping', title='Ping', desc="Entre com o IP")
+    return render_template('input.html', 200, otp=otp, path='ping', title='Ping', desc="Entre com o IP")
 
 @app.route('/ping', methods=['POST'])
 def ping_post():
@@ -52,19 +50,18 @@ def ping_post():
         otp = "Host ativo!"
     else:
         otp = "Host inativo!"
-    return render_template(getpage, otp=otp, title='Ping')
+    return render_template('input.html', 200, otp=otp, title='Ping')
 
 @app.route('/rsakey', methods=['GET'])
 def rsakey():
     otp=None
-    return render_template(getpage, otp=otp, path='rsakey', title='Gerar chave RSA', desc="Escreva \"GERAR\" para gerar uma nova chave RSA")
+    return render_template('input.html', 200, otp=otp, path='rsakey', title='Gerar chave RSA', desc="Escreva \"GERAR\" para gerar uma nova chave RSA")
 
 @app.route('/rsakey', methods=['POST'])
 def rsakey_post():
-    title='Gerar chave RSA'
     otp=None
     if str(request.form.get('output')) != "GERAR":
-        return render_template(getpage, otp=otp, title=title, desc="Escreva \"GERAR\" para gerar uma nova chave RSA")
+        return render_template('input.html', 200, otp=otp, title='Gerar chave RSA', desc="Escreva \"GERAR\" para gerar uma nova chave RSA")
     else: 
         key = RSA.generate(2048)
         f = open('rsa-key.pem','wb')
@@ -73,24 +70,24 @@ def rsakey_post():
         f = open('rsa-key.pem','r')
         otp = f.read()
         f.close()
-        return render_template(getpage, otp=otp, title=title)
+        return render_template('input.html', 200, otp=otp, title='Gerar chave RSA')
 
 @app.route('/gerachave', methods=['GET'])
 def gerasenha():
     otp=None
-    return render_template(getpage, otp=otp, path='gerachave', title='Gerador de chave', desc="Entre com a string para gerar a chave")
+    return render_template('input.html', 200, otp=otp, path='gerachave', title='Gerador de chave', desc="Entre com a string para gerar a chave")
 
 @app.route('/gerachave', methods=['POST'])
 def gerasenha_post():
     password = str.encode(request.form.get('output'))
     salt = os.urandom(32)
     otp = pbkdf2_hmac('sha256', password, salt, 100000)
-    return render_template(getpage, otp=otp.hex(), title='Gerador de chave')
+    return render_template('input.html', 200, otp=otp.hex(), title='Gerador de chave')
 
 @app.route('/geratoken', methods=['GET'])
 def geratoken():
     otp=None
-    return render_template(getpage, otp=otp, path='geratoken', title='Gerador de tokens JWT', desc="Entre com o payload")
+    return render_template('input.html', 200, otp=otp, path='geratoken', title='Gerador de tokens JWT', desc="Entre com o payload")
 
 @app.route('/geratoken', methods=['POST'])
 def geratoken_post(otp=None):
@@ -104,7 +101,7 @@ def geratoken_post(otp=None):
 @app.route('/consulta', methods=['GET'])
 def consulta():
     otp=None
-    return render_template(getpage, otp=otp, path='consulta', title='Exemplo de Consulta', desc="Procure por um telefone fornecendo o nome")
+    return render_template('input.html', 200, otp=otp, path='consulta', title='Exemplo de Consulta', desc="Procure por um telefone fornecendo o nome")
 
 @app.route('/consulta', methods=['GET', 'POST'])
 def consulta_post():
@@ -112,4 +109,4 @@ def consulta_post():
     query = "SELECT name, phone FROM users WHERE name = '"+str(request.form.get('output'))+"';"
     result = conn.execute(query)
     otp = result.fetchall()
-    return render_template(getpage, otp=otp, title='Exemplo de Consulta')
+    return render_template('input.html', 200, otp=otp, title='Exemplo de Consulta')
